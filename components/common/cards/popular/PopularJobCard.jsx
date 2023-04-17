8209bc61008025dc60c8ab7803eae759066e4baf
CISSE07 <sadibouc07@gmail.com>
2023-04-17 20:25:11 +0200

    UPDATE 17/04

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './popularjobcard.style'
+
+const PopularJobCard = () => {
+  return (
+    <View>
+      <Text>PopularJobCard</Text>
+    </View>
+  )
+}
+
+// 1:06 min
+
+export default PopularJobCard
